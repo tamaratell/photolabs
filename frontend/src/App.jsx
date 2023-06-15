7a42bf4494917eb3f844fdc1e0a296e756b670a4
Tamara Tell <2telltamara@gmail.com>
2023-06-15 16:20:23 -0400

    complete photoListItem component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,8 +6,8 @@ import './App.scss';
 // Note: Rendering a single component to build components in isolation
 const App = () => (
   <div className="App">
-    <PhotoListItem/>
+    <PhotoListItem />
   </div>
-)
+);
 
-export default App
+export default App;
--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -3,9 +3,30 @@ import React from 'react';
 
 import '../styles/PhotoListItem.scss';
 
-const PhotoListItem = () => {
-  /* Insert React */
-}
+const PhotoListItem = (props) => {
+  const { id, location, imageSource, username, profile } = props;
+
+  return (
+    <article className='photo-list__item' key={id}>
+
+      <img className='photo-list__image' src={imageSource} alt="image" />
+
+      <div className='photo-list__user-details'>
+
+        <div className="photo-list__user-info">{username}
+
+          <p className="photo-list__user-location">
+            {location.city}, {location.country}
+          </p>
+
+        </div>
+
+        <img src={profile} alt="user profile" className="photo-list__user-profile" />
+      </div>
+
+    </article>
+  );
+};
 
 PhotoListItem.defaultProps = {
   "id": "1",
@@ -16,6 +37,6 @@ PhotoListItem.defaultProps = {
   "imageSource": `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
   "username": "Joe Example",
   "profile": `${process.env.PUBLIC_URL}/profile-1.jpg`
-}
+};
 
-export default PhotoListItem
+export default PhotoListItem;
