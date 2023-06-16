4ec31f74afe2d589230be234ce7963d0f3ed131c
Tamara Tell <2telltamara@gmail.com>
2023-06-15 22:22:14 -0400

    add state function to PhotoFavButton

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -4,10 +4,17 @@ import { FavIcon } from './FavIcon';
 import '../styles/PhotoFavButton.scss';
 
 function PhotoFavButton() {
+
+  const [fav, setFav] = useState(false);
+
+  const clickFav = () => {
+    setFav(true);
+  };
+
   return (
     <div className="photo-list__fav-icon">
       <div className="photo-list__fav-icon-svg">
-        <FavIcon fill={"#C80000"} />
+        <FavIcon onClick={clickFav} fill={fav ? '#C80000' : '#EEEEEE'} />
       </div>
     </div>
   );
